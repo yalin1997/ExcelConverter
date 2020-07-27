8ad1c91d2e9052d6647b469666a4db18a0d80577
yalin1997 <godjj1997@gmail.com>
2020-07-27 22:08:04 +0800

    csvReader class

---
+++ ExcelConverter/CsvReaderClass.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Globalization;
+using System.IO;
+using System.Text;
+using CsvHelper;
+
+namespace ExcelConverter
+{
+    class CsvReaderClass
+    {
+        string csvPath { get; set; }
+
+        public CsvReaderClass(string path)
+        {
+            this.csvPath = path;
+        }
+        public void readCsv()
+        {
+            using (var reader = new StreamReader(csvPath))
+            {
+                using (var csv = new CsvReader(reader , CultureInfo.InvariantCulture))
+                {
+                    for (int i = 0; i < 7; i++)
+                    {
+                        csv.Read();
+                        Console.WriteLine(csv.Context);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ ExcelConverter/DataModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExcelConverter
+{
+    class DataModel
+    {
+        string Longitudinal_position{get;set;}
+        string Circumferential_position { get; set;}
+        string Depth { get; set; }
+    }
+}
